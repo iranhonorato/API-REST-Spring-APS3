fd461367bd19e981a8ec671ab2cd333b181b1cf3
iran <iranilsonhonorato88@gmail.com>
2025-10-14 17:42:36 -0300

    feat: Implementando MVC para cliente

--- src/main/java/com/example/API_REST_Spring_APS3/cliente/ClienteController.java
+++ src/main/java/com/example/API_REST_Spring_APS3/cliente/ClienteController.java
@@ -1,4 +1,50 @@
 package com.example.API_REST_Spring_APS3.cliente;
 
+import com.example.API_REST_Spring_APS3.usurario.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+
+
+@RestController
+@RequestMapping("/cliente")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public Collection<Cliente> listarClientesController() {
+        return clienteService.listarClientes();
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<?> buscarPorCpfController(@PathVariable String cpf) {
+        Cliente cliente = clienteService.buscarPorCpf(cpf);
+        if (cliente == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
+    }
+
+
+    @PutMapping("/{cpf}")
+    public ResponseEntity<?> editarClienteController(
+            @PathVariable String cpf,
+            @RequestBody Cliente cliente,
+            @RequestHeader("Authorization") String token) {
+        try {
+            usuarioService.validarToken(token);
+            Cliente clienteEditado = clienteService.editarCliente(cpf, cliente);
+            return ResponseEntity.ok(clienteEditado);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/example/API_REST_Spring_APS3/cliente/ClienteService.java
+++ src/main/java/com/example/API_REST_Spring_APS3/cliente/ClienteService.java
@@ -1,4 +1,45 @@
 package com.example.API_REST_Spring_APS3.cliente;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+@Service
 public class ClienteService {
+    private final Map<String, Cliente> clientes = new HashMap<>();
+
+
+    public Collection<Cliente> listarClientes() {return clientes.values();}
+
+
+    public Cliente buscarPorCpf(String cpf) {
+        return clientes.get(cpf);
+    }
+
+    public Cliente salvarCliente(Cliente cliente) {
+        if (cliente.getCpf() == null || cliente.getCpf().isEmpty()) {
+            throw new IllegalArgumentException("O CPF do cliente não pode ser nulo ou vazio");
+        }
+
+        if (clientes.containsKey(cliente.getCpf())) {
+            throw new IllegalArgumentException("Já existe um cliente com esse CPF");
+
+        }
+
+        clientes.put(cliente.getCpf(), cliente);
+        return cliente;
+    }
+
+    public Cliente editarCliente(String cpf, Cliente novosDados) {
+        if (!clientes.containsKey(cpf)) {
+            throw new IllegalArgumentException("Cliente não encontrado com CPF: " + cpf);
+        }
+        clientes.put(cpf, novosDados);
+        return novosDados;
+    }
+
 }
