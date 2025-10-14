c982da29ef29e4814e3296ac8fda04909b2ed1ae
iran <iranilsonhonorato88@gmail.com>
2025-10-14 17:44:38 -0300

    feat: Implementando MVC para cartao

--- src/main/java/com/example/API_REST_Spring_APS3/cartao/CartaoController.java
+++ src/main/java/com/example/API_REST_Spring_APS3/cartao/CartaoController.java
@@ -1,4 +1,63 @@
 package com.example.API_REST_Spring_APS3.cartao;
 
+
+//Cliente → envia requisição HTTP → Controller
+//Controller → chama métodos → Service
+//Service → usa/gera dados → Model (Autor)
+//Service → retorna resultado → Controller
+//Controller → devolve JSON → Cliente
+
+
+import com.example.API_REST_Spring_APS3.usurario.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+//Controller = entrada/saída da API.
+@RestController // Indica que a classe é um controller e que todos os métodos retornarão um JSON
+@RequestMapping("/cartoes")  // Define o caminho base da API: todos os endpoints começam com /cartoes.
 public class CartaoController {
+
+    @Autowired
+    private CartaoService cartaoService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public Collection<Cartao> listarCartoesController() {
+        return cartaoService.listarCartoes();
+    }
+
+    @GetMapping("/{numeroCartao}")
+    public Cartao buscarCartaoController(@PathVariable String numeroCartao) {
+        return cartaoService.buscarPorNumero(numeroCartao);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> salvarCartaoController(
+            @RequestBody Cartao cartao,
+            @RequestHeader("Authorization") String token) {
+        try {
+            usuarioService.validarToken(token);
+            Cartao novoCartao = cartaoService.salvarCartao(cartao);
+            return ResponseEntity.ok(novoCartao);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+    @DeleteMapping("/{numeroCartao}")
+    public ResponseEntity<?> deletarCartaoController(
+            @PathVariable String numeroCartao,
+            @RequestHeader("Authorization") String token) {
+        usuarioService.validarToken(token);
+        cartaoService.deletar(numeroCartao);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
--- src/main/java/com/example/API_REST_Spring_APS3/cartao/CartaoService.java
+++ src/main/java/com/example/API_REST_Spring_APS3/cartao/CartaoService.java
@@ -1,4 +1,49 @@
 package com.example.API_REST_Spring_APS3.cartao;
 
+//Cliente → envia requisição HTTP → Controller
+//Controller → chama métodos → Service
+//Service → usa/gera dados → Model (Autor)
+//Service → retorna resultado → Controller
+//Controller → devolve JSON → Cliente
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+//Service = regra de negócio.
+@Service
 public class CartaoService {
+    // Banco de dados em memória
+    private final Map<String, Cartao> cartoes = new HashMap<>();
+
+    public Collection<Cartao> listarCartoes() {
+        return cartoes.values();
+    }
+
+    public Cartao buscarPorNumero(String numeroCartao) {
+        return cartoes.get(numeroCartao);
+    }
+
+    public Cartao salvarCartao(Cartao cartao) {
+        if (cartao.getNumeroCartao() == null || cartao.getNumeroCartao().isEmpty()) {
+            throw new IllegalArgumentException("O número do cartão não pode ser nulo ou vazio.");
+        }
+        // Verifica duplicidade
+        if (cartoes.containsKey(cartao.getNumeroCartao())) {
+            throw new IllegalArgumentException("Já existe um cartão com esse número.");
+        }
+
+        cartoes.put(cartao.getNumeroCartao(), cartao);
+        return cartao;
+    }
+
+
+    public void deletar(String numeroCartao) {
+        cartoes.remove(numeroCartao);
+
+    }
+
 }
