810d6c4ba8f703027600172dc6c437b2152f29b6
iran <iranilsonhonorato88@gmail.com>
2025-10-14 17:46:17 -0300

    feat: Implementando MVC para contaCorrente

--- src/main/java/com/example/API_REST_Spring_APS3/contaCorrente/ContaCorrenteController.java
+++ src/main/java/com/example/API_REST_Spring_APS3/contaCorrente/ContaCorrenteController.java
@@ -1,4 +1,74 @@
 package com.example.API_REST_Spring_APS3.contaCorrente;
 
+import com.example.API_REST_Spring_APS3.movimentacao.Movimentacao;
+import com.example.API_REST_Spring_APS3.usurario.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/conta")
 public class ContaCorrenteController {
+
+    @Autowired
+    private ContaCorrenteService contaCorrenteService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public Collection<ContaCorrente> listarContasController(){ return  contaCorrenteService.listarContas(); }
+
+
+    @GetMapping("/{conta}")
+    public ArrayList<Movimentacao> listarMovimentacoesController(@PathVariable String conta){
+        return  contaCorrenteService.listarMovimentacoes(conta);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> cadastrarContaController(
+            @RequestBody ContaCorrente contaCorrente,
+            @RequestHeader("Authorization") String token){
+        try {
+            usuarioService.validarToken(token);
+            ContaCorrente novaConta = contaCorrenteService.cadastrarConta(contaCorrente);
+            return ResponseEntity.ok().body(novaConta);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+    @PostMapping("/{conta}/saque")
+    public ResponseEntity<?> saqueController(
+            @PathVariable String conta,
+            @RequestBody Float valor,
+            @RequestHeader("Authorization") String token){
+        try {
+            usuarioService.validarToken(token);
+            contaCorrenteService.sacar(conta, valor);
+            return ResponseEntity.ok().body("Saque realizado com sucesso");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+    @PostMapping("/{conta}/deposito")
+    public ResponseEntity<?> depositarController(
+            @PathVariable String conta,
+            @RequestBody Float valor,
+            @RequestHeader("Authorization") String token){
+        try {
+            usuarioService.validarToken(token);
+            contaCorrenteService.depositar(conta, valor);
+            return ResponseEntity.ok().body("Depósito realizado com sucesso");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/example/API_REST_Spring_APS3/contaCorrente/ContaCorrenteService.java
+++ src/main/java/com/example/API_REST_Spring_APS3/contaCorrente/ContaCorrenteService.java
@@ -1,4 +1,64 @@
 package com.example.API_REST_Spring_APS3.contaCorrente;
 
+
+import com.example.API_REST_Spring_APS3.cliente.ClienteService;
+import com.example.API_REST_Spring_APS3.movimentacao.Movimentacao;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class ContaCorrenteService {
+    private final Map<String, ContaCorrente> contas = new HashMap<>();
+    private final ClienteService clienteConta;
+
+    public ContaCorrenteService(ClienteService cliente) {
+        this.clienteConta = cliente;
+    }
+
+
+    public Collection<ContaCorrente> listarContas() {return contas.values();}
+
+    public ContaCorrente cadastrarConta(ContaCorrente conta) {
+        if (conta.getConta() == null || conta.getConta().isBlank()) {
+            throw new IllegalArgumentException("O número da conta não pode ser nulo ou vazio");
+        }
+
+        if (contas.containsKey(conta.getConta())) {
+            throw new IllegalArgumentException("Já existe uma conta com esse número corrente");
+        }
+
+        clienteConta.salvarCliente(conta.getCliente());
+        contas.put(conta.getConta(), conta);
+        return conta;
+    };
+
+    public ArrayList<Movimentacao> listarMovimentacoes(String conta) {
+        ContaCorrente contaCorrente = contas.get(conta);
+        if (contaCorrente == null) {
+            throw new IllegalArgumentException("Conta não encontrada");
+        }
+        return contaCorrente.listaMovimentacoes();
+    }
+
+
+    public void depositar(String conta, Float valor) {
+        ContaCorrente contaCorrente = contas.get(conta);
+        if (contaCorrente == null) {
+            throw new IllegalArgumentException("Conta não encontrada");
+        }
+        contaCorrente.deposito(valor);
+    }
+
+    public void sacar(String conta, Float valor) {
+        ContaCorrente contaCorrente = contas.get(conta);
+        if (contaCorrente == null) {
+            throw new IllegalArgumentException("Conta não encontrada");
+        }
+        contaCorrente.saque(valor);
+    }
+
 }
