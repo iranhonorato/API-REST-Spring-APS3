9c96a535fe60bc3131755e011fe22958b8276abc
iran <iranilsonhonorato88@gmail.com>
2025-10-14 17:30:32 -0300

    feat: Implementando criptografia de senhas e implementando MVC para usuário

--- src/main/java/com/example/API_REST_Spring_APS3/usurario/UsuarioController.java
+++ src/main/java/com/example/API_REST_Spring_APS3/usurario/UsuarioController.java
@@ -1,4 +1,29 @@
 package com.example.API_REST_Spring_APS3.usurario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.Collection;
+
+
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public Usuario cadastrarUsuarioController(@RequestBody Usuario usuario) {
+        return usuarioService.cadastrarUsuario(usuario);
+    }
+
+    @GetMapping
+    public Collection<Usuario> listarUsuarioController() {
+        return usuarioService.listarUsuarios();
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Usuario usuario) {
+        return usuarioService.login(usuario);
+    }
 }
--- src/main/java/com/example/API_REST_Spring_APS3/usurario/UsuarioService.java
+++ src/main/java/com/example/API_REST_Spring_APS3/usurario/UsuarioService.java
@@ -1,4 +1,68 @@
 package com.example.API_REST_Spring_APS3.usurario;
 
+import org.mindrot.jbcrypt.BCrypt;
+// Para utilizar o import acima você precisa fazer:
+//<dependency>
+//        <groupId>org.mindrot</groupId>
+//        <artifactId>jbcrypt</artifactId>
+//        <version>0.4</version>
+//</dependency>
+
+
+
+
+import org.springframework.stereotype.Service;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.UUID;
+
+
+
+@Service
 public class UsuarioService {
+    private final HashMap<String, Usuario> usuariosDB = new HashMap<>();
+    private final HashMap<String, Usuario> tokensDB = new HashMap<>();
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        String password = usuario.getPassword();
+//        gensalt() gera um salt aleatório. O salt é um valor aleatório acrescentado à senha antes do hash
+//        hashpw() aplica o hash e já armazena a senha de forma segura.
+        usuario.setPassword(BCrypt.hashpw(password, BCrypt.gensalt()));
+        usuariosDB.put(usuario.getEmail(), usuario);
+        return usuario;
+    };
+
+    public Collection<Usuario> listarUsuarios() {
+        return usuariosDB.values();
+    };
+
+
+    public String login(Usuario usuario) {
+        Usuario userDB = usuariosDB.get(usuario.getEmail());
+
+
+//        BCrypt.checkpw(senhaDigitada, senhaArmazenada);
+        if (BCrypt.checkpw(usuario.getPassword(), userDB.getPassword())) {
+//            O BCrypt sabe verificar se a senha digitada gera o mesmo hash (considerando o salt).
+//            Se bater, a senha está correta.
+
+            String token = UUID.randomUUID().toString();
+            tokensDB.put(token, usuario);
+            return token;
+        }
+        throw new RuntimeException("Usuário ou senha inválidos");
+    }
+
+
+    public Usuario validarToken(String token) {
+        Usuario usuario = tokensDB.get(token);
+
+        if (usuario == null) {
+            throw new RuntimeException("Token Inválido");
+        }
+        return usuario;
+    }
+
+
+
 }
